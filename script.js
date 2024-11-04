4e88f1a337a0c32c583352149d2afbc1cafeefb8
gonzalez-7 <20agonzalezp@gmail.com>
2024-11-03 23:01:54 -0800

    First task completed with a function min that takes two numbers and returns the smallest one of the pair.

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
+function min(a, b) {
+    return a < b ? a : b;
+}
+console.log(min(0, 10)); // will output 0
+console.log(min(0, -10)); // will output -10
+
+
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
